6cbd1f2baa5589e4ed0e8ee98dc89443b765519c
Galang Piliang <galang.pil@gmail.com>
2020-01-30 17:38:22 +0700

    [Galang] - Update Find Most Char

--- findMostChar.js
+++ findMostChar.js
@@ -1,8 +1,10 @@
+// Cara satu, caraku
 let findMostChar = function(str){
     let max = 0
     let mostChar = ''
     for(i=0; i < str.length; i++){
       lengthSplit = str.split(str[i]).length
+      console.log(str.split(str[i]))
       if(lengthSplit > max){
         max = lengthSplit
         mostChar = str[i]
@@ -11,4 +13,28 @@ let findMostChar = function(str){
     return mostChar
   }
   
-  console.log(findMostChar('a1a11b1b221133aa'))
+  // console.log(findMostChar('aabac'))
+  
+  // Cara dua, Mas Yudi
+  function check(str) {
+    // cari jumlah
+    let total = []
+    for(let i=0; i < str.length; i++) {
+      if(total[str[i]]) {
+        total[str[i]]++
+      }else{
+        total[str[i]] = 1
+      }
+    }
+    // console.log(total)
+    let sortTotal = total.sort();
+    let max = sortTotal[sortTotal.length-1]
+  
+    for(let i=0; i<str.length; i++) {
+      if(total[str] === max){
+        return str[i]
+      }
+    }
+  }
+  
+  console.log(check('aaba'))
